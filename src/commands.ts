28e4870e1905cf14959173a0ccdadf6051a8e101
medicareamaze <37244619+medicareamaze@users.noreply.github.com>
2018-03-17 08:04:38 -0700

    Update commands.ts

--- src/commands.ts
+++ src/commands.ts
@@ -96,12 +96,10 @@ async function agentCommand(
 
 async function customerCommand(session: builder.Session, next: Function, handoff: Handoff) {
     const message = session.message;
-    let conversation = await handoff.getConversation({ customerConversationId: message.address.conversation.id }, message.address);        
-    conversation.customer.user = message.user;   
     const customerStartHandoffCommandRegex = new RegExp("^" + indexExports._customerStartHandoffCommand + "$", "gi");
     if (customerStartHandoffCommandRegex.test(message.text)) {
         // lookup the conversation (create it if one doesn't already exist)
-        //const conversation = await handoff.getConversation({ customerConversationId: message.address.conversation.id }, message.address);
+        const conversation = await handoff.getConversation({ customerConversationId: message.address.conversation.id }, message.address);
         if (conversation.state == ConversationState.Bot) {
             await handoff.addToTranscript({ customerConversationId: conversation.customer.conversation.id }, message);
             await handoff.queueCustomerForAgent({ customerConversationId: conversation.customer.conversation.id });
