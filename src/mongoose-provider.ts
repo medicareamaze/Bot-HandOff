b357c0897b82038b2169e0d4647ab3bf13e4904c
Sanjeev Rangaiah <Sanjeev.Rangaiah@tranzact.net>
2018-06-01 08:31:23 -0700

    bug fix

--- src/mongoose-provider.ts
+++ src/mongoose-provider.ts
@@ -359,11 +359,13 @@ export class MongooseProvider implements Provider {
             }
 
             LeadModel.findByIdAndUpdate((lead as any)._id, update).then((error) => {
-                if (!session.dialogData.data )
-                session.dialogData['data'] = {};
-                for (var prop in lead) {
-                    if (!session.dialogData.data[prop])
-                        session.dialogData.data[prop] = '';//initializing Session Dialog data for Graph Dialog to load  
+                if(session.dialogData) {
+                    if (!session.dialogData.data )
+                    session.dialogData['data'] = {};
+                    for (var prop in lead) {
+                        if (!session.dialogData.data[prop])
+                            session.dialogData.data[prop] = '';//initializing Session Dialog data for Graph Dialog to load  
+                    }
                 }
                 resolve(true)
             }).catch((error) => {
